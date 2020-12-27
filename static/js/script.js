b05359bb819643ef19dd90492fa9ca17b7c4eadf
Richard Byrne <richardbyrne1995@gmail.com>
2020-12-27 21:26:28 +0000

    Add occassion or location based on wildflower checkbox

--- static/js/script.js
+++ static/js/script.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
         $(this).css("filter", "brightness(100%)");
         $(this).siblings("button").hide();
     })
+
+    // Add Flower Form
+    $("input[id='is_wildflower']").click(function() {
+        if($(this).is(":checked")){
+            $(".location").addClass("d-block")
+            $(".location").removeClass("d-none")
+            $(".occasions").addClass("d-none")
+            $(".occasions").removeClass("d-block")
+        } else {
+            $(".location").addClass("d-none")
+            $(".location").removeClass("d-block")
+            $(".occasions").addClass("d-block")
+            $(".occasions").removeClass("d-none")
+        }
+    })
 })
