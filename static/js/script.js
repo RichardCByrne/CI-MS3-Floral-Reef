63d69213707bd1fdd5a39aee60642d1df91698ed
Richard Byrne <richardbyrne1995@gmail.com>
2020-12-27 18:30:33 +0000

    Center image button when imaged not owned by current user

--- static/js/script.js
+++ static/js/script.js
@@ -10,12 +10,4 @@ $(document).ready(function(){
         $(this).css("filter", "brightness(100%)");
         $(this).siblings("button").hide();
     })
-
-    $(".user-image-view-btn").hover(function(){
-        $(this).css("filter", "brightness(50%)");
-        $(this).siblings("button").show();
-    },function(){
-        $(this).css("filter", "brightness(100%)");
-        $(this).siblings("button").hide();
-    })
 })
